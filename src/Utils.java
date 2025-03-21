684c2bd037706311c92de5f5374d61bcd4f2ac8f
Josiah Randleman <69489368+jrandl@users.noreply.github.com>
2025-03-21 17:58:58 -0500

    Create Utils.java

---
+++ src/Utils.java
@@ -0,0 +1,9 @@
+public class Utils {
+    public static boolean isPositive(int number) {
+        return number > 0;
+    }
+
+    public static boolean isEven(int number) {
+        return number % 2 == 0;
+    }
+}
